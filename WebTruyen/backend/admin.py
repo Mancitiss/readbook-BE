fcbd4d24cdcc294ff4fb01152222f21287bbe9ea
Phan Thi Linh <20520615@gm.uit.edu.vn>
2022-10-22 03:16:04 +0700

    update admin page:

--- WebTruyen/backend/admin.py
+++ WebTruyen/backend/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import LichSu, BinhLuan, YeuThich, Chuong, Truyen, TheLoai, User
 # Register your models here.
+admin.site.register(LichSu)
+admin.site.register(BinhLuan)
+admin.site.register(YeuThich)
+admin.site.register(Chuong)
+admin.site.register(Truyen)
+admin.site.register(TheLoai)
+admin.site.register(User)
+
