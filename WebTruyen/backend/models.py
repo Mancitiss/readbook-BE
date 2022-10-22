b8582973aafb3af1cf81defc93d8c2505cacd2df
Phan Thi Linh <20520615@gm.uit.edu.vn>
2022-10-22 15:30:37 +0700

    update model Love comment + bookreview

--- WebTruyen/backend/models.py
+++ WebTruyen/backend/models.py
@@ -1,61 +1,70 @@
 from ctypes import addressof
+from email.policy import default
+from enum import unique
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
 class User(AbstractUser):
-    avatar = models.ImageField(upload_to='uploads/%Y/%m')
-    intro = models.CharField(max_length=255, null=True)
     nickname = models.CharField(max_length=255, null=True)
+    avatar = models.ImageField(upload_to='uploads/%Y/%m', null=True)
+    intro = models.CharField(max_length=255, null=True)
     address = models.TextField(null=True, blank=True)
     hobbies = models.TextField(null=True, blank=True)
     phone = models.CharField(max_length=20, null=True)
 class Category(models.Model):
-    # tl_id
-    category_name = models.CharField(max_length=255, null=False)
+    category_name = models.CharField(max_length=255, null=False, unique=True)
     def __str__(self):
         return self.category_name
     
 class Story(models.Model):
-    # tr_id 
-    category_name = models.ManyToManyField('Category', blank=True, null=True)
     story_name = models.CharField(max_length=255, null=False)
+    category_name = models.ManyToManyField('Category', blank=True)
     create_date = models.DateTimeField(auto_now_add=True)
     author = models.CharField(max_length=255, null=False)
-    image = models.ImageField(upload_to='uploads/%Y/%m')
+    image = models.ImageField(upload_to='uploads/%Y/%m', blank=True, null=True)
     total_chapters = models.IntegerField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     showtimes = models.TextField(null=True, blank=True)
-    likes = models.IntegerField()
-    rating = models.DecimalField(max_digits=5, decimal_places=2)
-    views = models.IntegerField()
+    rating = models.DecimalField(max_digits=5, decimal_places=2, default=0)
+    views = models.IntegerField(default=0)
     introduce = models.TextField(null=True, blank=True)
 
-    def __str__(self):
-        return self.category_name
-
 class Chapter(models.Model):
-    # ch_id
     story = models.ForeignKey(Story, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     chapter_name = models.CharField(max_length=255, null=False)
     content = models.TextField(null=True, blank=True)
 
-class LoveStory(models.Model):
+class SaveStory(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     story = models.ForeignKey(Story, on_delete=models.CASCADE)
     create_date = models.DateTimeField(auto_now_add=True)
 
 class Comment(models.Model):
-    # bl_id 
     story = models.ForeignKey(Story, on_delete=models.CASCADE)
+    previous_comment = models.ForeignKey('Comment', on_delete=models.CASCADE, null=True, blank=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     chapter = models.ForeignKey(Chapter, on_delete=models.CASCADE)
     content = models.TextField(null=True, blank=True)
     create_date = models.DateTimeField(auto_now_add=True)
 
 class History(models.Model):
+    story = models.ForeignKey(Story, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     create_date = models.DateTimeField(auto_now_add=True)
     chapter = models.ForeignKey(Chapter, on_delete=models.CASCADE)
+
+class LoveComment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    comment = models.ForeignKey(Comment, on_delete=models.CASCADE)
+    create_date = models.DateTimeField(auto_now_add=True)
+    like_dislike = models.BooleanField(default=0)
+
+class BookReview(models.Model):
     story = models.ForeignKey(Story, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField(null=True, blank=True)
+    create_date = models.DateTimeField(auto_now_add=True)
+    like = models.IntegerField(default=0)
+    
