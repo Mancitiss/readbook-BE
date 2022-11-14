98da62c7ba129f27dc61ea4f4b060500a5732fc3
Phan Thi Linh <20520615@gm.uit.edu.vn>
2022-11-14 11:08:00 +0700

    update api getOneUser(id) CreateUser()

---
+++ WebTruyen/backend/serializers.py
@@ -0,0 +1,22 @@
+
+from rest_framework.serializers import ModelSerializer
+from .models import Category, User
+
+class UserSerializer(ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'first_name', 'last_name', 'email', 'username', 'password', 'avatar']
+        extra_kwargs = {
+            'password': {'write_only': 'true'}
+        }
+
+    def create(self, validated_data):
+        user = User(**validated_data)
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
+
+class CategorySerializer(ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ["category_name"]
