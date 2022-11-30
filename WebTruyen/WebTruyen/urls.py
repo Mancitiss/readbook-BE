813b31483aee7ab17bd0d2c7d8b6aaa7553d32ea
Phan Thi Linh <20520615@gm.uit.edu.vn>
2022-11-30 21:54:21 +0700

    update api chapter + story

--- WebTruyen/WebTruyen/urls.py
+++ WebTruyen/WebTruyen/urls.py
@@ -37,8 +37,6 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin', admin.site.urls),
-    path('api/chapter/', views.Chapter_list),
-    path('api/story/', views.Story_list),
     path('api/', include('backend.urls')),
     path('o/', include('oauth2_provider.urls', namespace='oauth2_provider')),
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui (cache_timeout=0), name='schema-json'),
--- WebTruyen/backend/urls.py
+++ WebTruyen/backend/urls.py
@@ -5,6 +5,8 @@ from rest_framework.routers import DefaultRouter
 router = DefaultRouter()
 router.register('categorys', views.CategoryViewSet)
 router.register('users', views.UserViewSet)
+router.register('story', views.StoryViewSet)
+router.register('chapter', views.ChapterViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
