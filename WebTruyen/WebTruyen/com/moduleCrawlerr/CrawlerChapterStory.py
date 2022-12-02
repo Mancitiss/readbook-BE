c80057ecd220671926f07c35ef6c2007c18891ed
khoaphamkim12 <khoaphamkim12>
2022-12-02 18:58:42 +0700

    update craler

--- WebTruyen/WebTruyen/com/moduleCrawlerr/CrawlerChapterStory.py
+++ WebTruyen/WebTruyen/com/moduleCrawlerr/CrawlerChapterStory.py
@@ -15,13 +15,15 @@ class CrawlerChapterStory():
         CrawlerChapterStory.getNameStoryUserCategory()
 
     def getNameStoryUserCategory():
+
         with open('storyname.csv', mode='r') as csv_file:
             csv_reader = csv.DictReader(csv_file)
-            for row in csv_reader:
+            for (row) in csv_reader:
                 CrawlerChapterStory.getName(row)
                 time.sleep(1)
 
     def getName(row):
+        index = 0
         url = row['linkstory']
         try:
             x = requests.get(url)
@@ -30,18 +32,21 @@ class CrawlerChapterStory():
             elements = soup.select('#list-chapter .row .list-chapter li')
             arrayList = []
             for x in elements:
-
-                objectItem = {}
-                objectItem['story'] = row['story_name']
-                objectItem['chapter_name'] = x.select_one('a').attrs['title']
-                objectItem['linkstory'] = x.select_one('a').attrs['href']
-                x = requests.get(objectItem['linkstory'])
-                soup = BeautifulSoup(x.text, "html.parser")
-                element = soup.select_one('#chapter-c')
-                objectItem['content'] = element.text
-                print(objectItem)
-                arrayList.append(objectItem)
-                CrawlerChapterStory.inserData(arrayList)
+                if (index <= 5):
+                    index = index +1
+                    objectItem = {}
+                    objectItem['story'] = row['story_name']
+                    objectItem['chapter_name'] = x.select_one('a').attrs['title']
+                    objectItem['linkstory'] = x.select_one('a').attrs['href']
+                    x = requests.get(objectItem['linkstory'])
+                    soup = BeautifulSoup(x.text, "html.parser")
+                    element = soup.select_one('#chapter-c')
+                    objectItem['content'] = element.text
+                    # print(objectItem)
+                    arrayList.append(objectItem)
+                    CrawlerChapterStory.getName(objectItem)
+            CrawlerChapterStory.inserData(arrayList)
+                   
         except:
             print("An exception occurred")
 
